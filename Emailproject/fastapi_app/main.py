705dfe2124c8e6c1ca584d092d494ff0e28da0a0
Hari6900 <harikrishnan6900@gmail.com>
2025-12-09 17:30:58 +0530

    Profile module, schemas, and migration added

--- Emailproject/fastapi_app/main.py
+++ Emailproject/fastapi_app/main.py
@@ -3,9 +3,8 @@ from .django_setup import setup_django
 setup_django()
 from fastapi_app.routers import auth, users, email, chat, analytics
 
-from fastapi_app.routers import task  # IMPORT
-
-    
+from fastapi_app.routers import task  
+from fastapi_app.routers import profile
 
 
 app = FastAPI()
@@ -13,8 +12,10 @@ app.include_router(email.router, prefix="/email", tags=["Emails"])
 app.include_router(auth.router, prefix="/auth", tags=["Authentication"])
 app.include_router(users.router, prefix="/users", tags=["Users"])
 app.include_router(chat.router, prefix="/chat", tags=["Chat"])
-app.include_router(task.router)                                                # REGISTER
+app.include_router(task.router)                                                
 app.include_router(analytics.router, prefix="/analytics", tags=["Analytics"])
+app.include_router(profile.router)
+
 @app.get("/")
 def read_root():
     return {"message": "Django and FastAPI are linked!"}
